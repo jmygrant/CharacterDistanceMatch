e63f998f18b452668abcb82ec2120ec340420279
Jared Mygrant <jamrow@gmail.com>
2018-05-01 16:44:48 -0600

    01 - Calculate the sum of characters that are unique.

---
+++ CharacterMatchSum/CharacterMatchSum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CharacterMatchSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int result = CharacterMismatchSum("cat", "at");
+			Console.WriteLine(string.Format("{0}", result));
+
+			result = CharacterMismatchSum("boat", "got");
+			Console.WriteLine(string.Format("{0}", result));
+
+			result = CharacterMismatchSum("though", "sloughs");
+			Console.WriteLine(string.Format("{0}", result));
+
+			Console.ReadLine();
+        }
+
+		public static int CharacterMismatchSum(string str1, string str2)
+		{
+			int sum = 0;
+			string mergeString = str1;
+
+			for(int i = 0; i < str2.Length; i++)
+			{
+				if(mergeString.Contains(str2[i].ToString()))
+				{
+					int index = mergeString.IndexOf(str2[i]);
+					mergeString = mergeString.Remove(index, 1);
+				}
+				else
+				{
+					mergeString += str2[i];
+				}
+			}
+
+			for(int i = 0; i<mergeString.Length; i++)
+			{
+				sum += Convert.ToInt32(mergeString[i]);
+			}
+
+			return sum;
+		}
+    }
+}
